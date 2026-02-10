c28c4039d9cc0c5dce570a27df41cdf467579cab
AshishGhodvinde <ghodvindeashish@gmail.com>
2026-02-10 21:52:07 +0530

    Day 48

---
+++ 1678.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public String interpret(String command) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<command.length(); i++){
+            if(command.charAt(i)=='G'){
+                sb.append('G');
+            }else if(command.charAt(i)=='(' && command.charAt(i+1)==')'){
+                sb.append('o');
+                i++;
+            }else{      //(al) = al
+                sb.append("al");
+                i+=3;
+            }
+        }
+        return sb.toString();
+    }
+}
