3ae1a4708df9c755f52f4521dd36af3b91998e94
AshishGhodvinde <ghodvindeashish@gmail.com>
2026-04-28 22:34:39 +0530

    Leet 2000

---
+++ 2000.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int idx = word.indexOf(ch);
+        if(ch==-1)  return word;
+
+        StringBuilder sb = new StringBuilder(word.substring(0, idx+1));     //+1 for inclusive char
+        sb.reverse();
+
+        return sb.toString()+word.substring(idx+1);
+    }
+}
