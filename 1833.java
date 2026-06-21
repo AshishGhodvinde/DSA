fe61c9846246406da7f3a9f8256a5c09bb856aee
AshishGhodvinde <ghodvindeashish@gmail.com>
2026-06-21 21:16:19 +0530

    Leet 1833

---
+++ 1833.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int cnt = 0;
+        for(int i=0; i<costs.length; i++){
+            if(costs[i] <= coins){
+                cnt++;
+                coins-=costs[i];
+            }
+        }
+        return cnt;
+    }
+}
