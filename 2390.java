f01879c9626d62f3c6d4f9195be352cce87dde91
AshishGhodvinde <ghodvindeashish@gmail.com>
2026-06-22 08:52:31 +0530

    Leet 2390 - stack

---
+++ 2390.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public String removeStars(String s) {
+        // Stack<Character> stack = new Stack<>();
+        // for(int i=0; i<s.length(); i++){
+        //     char ch = s.charAt(i);
+        //     if(ch == '*'){
+        //         stack.pop();
+        //     }else{
+        //         stack.push(ch);
+        //     }
+        // }
+        // StringBuilder sb = new StringBuilder();
+        // for(char ch : stack){
+        //     sb.append(ch);
+        // }
+        // return sb.toString();
+
+        StringBuilder sb = new StringBuilder();
+        for(char ch : s.toCharArray()){
+            if(ch == '*'){
+                sb.setLength(sb.length()-1);
+            }else{
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
