89fb91055e012cc4e769aa1c12145a7cbc60665b
AshishGhodvinde <ghodvindeashish@gmail.com>
2026-07-04 21:19:00 +0530

    Leet 1394

---
+++ 1394.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findLucky(int[] arr) {
+        // Map<Integer, Integer> map = new HashMap<>();
+        // for(int num : arr){
+        //     map.put(num, map.getOrDefault(num, 0)+1);
+        // }
+        // int ans = -1;
+        // for(int key : map.keySet()){
+        //     if(key == map.get(key) && key>ans)  ans = key;
+        // }
+        // return ans;
+
+        int[] freq = new int[501];
+        for(int num : arr){
+            freq[num]++;
+        }
+        for(int i=500; i>=1; i--){
+            if(i == freq[i]) return i;
+        }
+        return -1;
+    }
+}
