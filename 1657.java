2377ccfa0b1bca6f6c1d8f55db7ad169da8f6b4e
AshishGhodvinde <ghodvindeashish@gmail.com>
2026-07-17 23:00:38 +0530

    Leet 1657

---
+++ 1657.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        int[] frq1 = new int[26];
+        int[] frq2 = new int[26];
+
+        for(int i=0; i<word1.length(); i++){
+            frq1[word1.charAt(i)-'a']++;
+        }
+
+        for(int i=0; i<word2.length(); i++){
+            frq2[word2.charAt(i)-'a']++;
+        }
+
+        for(int i=0; i<26; i++){
+            if(frq1[i]==0 && frq2[i]!=0 || frq1[i]!=0 && frq2[i]==0)    return false;
+        }
+
+        Arrays.sort(frq1);
+        Arrays.sort(frq2);
+
+        // for(int i=0; i<26; i++){
+        //     if(frq1[i] != frq2[i]) return false;
+        // }
+        // return true;
+
+        return Arrays.equals(frq1, frq2);
+    }
+}
+
+
+// if the number of occurences are equal then we can modify them with the given operations
